c049e4c1f20c7542882efa8ea1a02c9f83c0aa90
Joel Dietz <jdietz@71-23-192-45.par.clearwire-wmx.net>
2011-01-14 13:40:34 -0500

    init'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCinch::Application.config.secret_token = 'bb836a6c904642e00fc4d5abfe25052fbdbb34702cf90f071add9d5b5f0f7111dd114138378f368a2a771a13200474b06801be7281632bafd15ed820420670b1'
