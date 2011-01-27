94dbb120152aa40f32537752c90c441b20404591
Joel Dietz <jdietz@71-23-192-45.par.clearwire-wmx.net>
2011-01-27 17:19:44 -0500

    change app name

--- config/initializers/typus.rb
+++ config/initializers/typus.rb
@@ -1,7 +1,7 @@
 Typus.setup do |config|
 
   # Application name.
-  config.admin_title = "Saasy Logger"
+  config.admin_title = "Unofficial Salesforce Knowledge Base"
   # config.admin_sub_title = ""
 
   # When mailer_sender is set, password recover is enabled. This email
